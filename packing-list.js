d85943d6ad8519a9ecc4619c6727e9b474ab0ac1
Joseph Ng <josephng81work@gmail.com>
2019-12-16 21:14:37 +0000

    Camping Packing list

---
+++ packing-list.js
@@ -0,0 +1,19 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+console.log("Kitchen stuff to pack:");
+
+//first eg...
+
+//for (let i = 0; i < packingList.length; i ++) {
+//console.log(packingList[i]);
+//}
+
+//second eg...
+
+let i = 0;
+
+while(i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+packingList[3];
